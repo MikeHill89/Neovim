c2cab9867d8791082be0060cf2d4f883bb981bf1
MikeHill89 <mvreugdenhil18@gmail.com>
2023-11-18 11:30:00 +0200

    Added Telescope keymap for viewing lsp document symbols

--- lua/mikehill89/plugins/telescope.lua
+++ lua/mikehill89/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
     keymap.set("n", "<leader>sf", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>rf", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>gf", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>gs", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>vs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "List all LSP Document Symbols" })
   end,
 }
